bb3fa291d9744f6d3c12055cea2bd918937bf0ca
Kristian Petrakovic <kristiankroatien@gmail.com>
2022-10-12 08:35:20 +0000

    1st block

---
+++ block.js
@@ -0,0 +1,9 @@
+class Block{
+    constructor(timestamp,lastHash,hash,data){
+
+        this.timestamp = timestamp; // Zeitstempel
+        this.lastHash = lastHash; // Hash des vorhergehenden Blocks
+        this.hash = hash; // Eigener Hash
+        this.data = data; // Daten
+    }
+}
